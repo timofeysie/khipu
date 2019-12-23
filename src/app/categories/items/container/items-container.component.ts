75bc7903439ae476f0904aa6b1820f1ac759ae3d
Timothy Curchod <timofeyc@hotmail.com>
2019-12-24 06:53:56 +1100

    #1 fixed merge conflicts with feature#12 branch

--- src/app/categories/items/container/items-container.component.ts
+++ src/app/categories/items/container/items-container.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemsStore } from '../items.store';
 import { ActivatedRoute } from '@angular/router';
+<<<<<<< HEAD
+=======
+import { Category } from '@app/core/interfaces/categories';
+>>>>>>> e043ee3bf7588420ccf7530fffac1d07a77291f9
 
 @Component({
   selector: 'app-items-container',
@@ -9,9 +13,17 @@ import { ActivatedRoute } from '@angular/router';
   providers: [ItemsStore]
 })
 export class ItemsContainerComponent implements OnInit {
+<<<<<<< HEAD
   constructor(private store: ItemsStore, private activeRoute: ActivatedRoute) {}
 
   ngOnInit() {
     console.log(this.activeRoute.queryParams);
   }
+=======
+  constructor(private store: ItemsStore, private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.params.subscribe((category: Category) => console.log(category));
+  }
+
+  ngOnInit() {}
+>>>>>>> e043ee3bf7588420ccf7530fffac1d07a77291f9
 }
